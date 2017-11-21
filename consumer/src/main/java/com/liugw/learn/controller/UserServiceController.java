f7a4e7baaa78181f4e35e896906abf3c8794f7cc
liugaowei999 <187346454@qq.com>
2017-11-21 09:58:08 +0800

    Change-Id: I7584473e52653afa0b2e2b6c7411f21f506ff947 Signed-off-by: liugaowei999 <187346454@qq.com>

--- consumer/src/main/java/com/liugw/learn/controller/UserServiceController.java
+++ consumer/src/main/java/com/liugw/learn/controller/UserServiceController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.liugw.provider.service.dubbotest.StudentService;
 import com.liugw.provider.service.dubbotest.UserService;
 
 @Controller
@@ -14,11 +15,23 @@ public class UserServiceController {
 	@Autowired
 	UserService userService;
 
+	@Autowired
+	StudentService studentService;
+
 	@RequestMapping("/user/{userid}")
 	@ResponseBody
 	public String getUser(@PathVariable int userid) {
 		System.out.println("Start query the user. " + userid);
 		String user = userService.getUserNamebyId(userid);
+		return user;
+	}
+
+	@RequestMapping("/student/{userid}")
+	@ResponseBody
+	public String getStudent(@PathVariable String userid) {
+		System.out.println("Start query the user. " + userid);
+		String user = studentService.getUserNamebyId(userid) + ":" + studentService.getSexByUserId(userid) + ":"
+				+ studentService.getScoreByUserId(userid);
 		return user;
 	}
 
